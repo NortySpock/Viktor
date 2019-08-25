7a2c07277d35263ea9314806d113f5d7d898022f
David Norton <david.k.norton@gmail.com>
2019-08-25 12:10:37 -0500

    merged in GunCooldown as it does not really need its own class

--- src/main.js
+++ src/main.js
@@ -660,3 +660,28 @@ function calculateHitPercentage()
        return ((Global.FinalPlayerShotsHit / Global.FinalPlayerShotsTotal)*100).toFixed(2);
     }
 }
+
+class GunCooldown
+{
+  //set number of frames this needs to cool down
+  constructor(frameCooldown)
+  {
+    //set size and fall rate
+    this.minCooldown = frameCooldown;
+    this.lastFiredFrame = 0;
+  }
+
+  canFire(currentFrame)
+  {
+    if(currentFrame >=  (this.lastFiredFrame+this.minCooldown))
+    {
+        return true;
+    }
+    return false;
+  }
+
+  fire(currentFrame)
+  {
+      this.lastFiredFrame = currentFrame;
+  }
+}
